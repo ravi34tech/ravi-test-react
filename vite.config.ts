309e26fedfe3a1600248e168d6639fd072a5cd91
Ravi Mummadu <rmummadu@thelcfgroup.com>
2025-06-02 13:15:18 +0530

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ravitestreact/'
+  base: '/ravitestreact/',
+  build: {
+    lib: {
+      entry: "index.html",
+      name: "ravitestreact",
+      fileName: (format) => `ravitestreact.${format}.js`,
+    },
+    target: "esnext",
+  },
 })
